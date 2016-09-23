510d1e4e81d19fdb02e8aa2ce4a9b42d928239a4
Mohamed Najd <yakosay@solidsnake.dyn.wpi.edu>
2016-09-23 16:02:46 -0400

    Backend Outlined

--- backend/LitRevBackend/public/index.php
+++ backend/LitRevBackend/public/index.php
@@ -13,6 +13,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 // Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
@@ -26,5 +28,60 @@ require __DIR__ . '/../src/middleware.php';
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
+// Display all projects associated with a user
+$app->get('/{usrid}/projects', function(Request $request, Response $response){
+
+});
+//---------------------------------------------------------------------------------Project
+//Get All Information associated with a research project
+$app->get('/project/{projectid}', function(Request $request, Response $response){
+
+});
+// Add a new research project.
+$app->post('/project', function (Request $request,Response $response){
+
+});
+// Modify an Existing Project
+$app->post('/project/{projectid}', function(Request $request, Response $response){
+
+});
+//DELETE research project
+$app->get('/project/{projectid}/del', function(Request $request, Response $response){
+
+});
+// SEARCH Project by Title
+$app->get('/search/project/{title}',function(Request $request, Response $response){
+
+});
+//GENERATE a BibTeXFile
+$app->get('/project/{projectid}/BibTeX', function(Request $request, Response $response){
+
+});
+//-------------------------------------------------------------------------------------Paper
+//Get A Paper
+$app->get('/project/{projectid}/paper/{paperid}', function(Request $request, Response $response){
+
+});
+// Add a new paper to a project
+$app->post('/project/{projectid}/paper', function (Request $request,Response $response){
+
+});
+// Modify an Existing Paper in a Project
+$app->post('/project/{projectid}/paper/{paperid}', function(Request $request, Response $response){
+
+});
+//DELETE research paper
+$app->get('/project/{projectid}/paper/{paperid}/del', function(Request $request, Response $response){
+
+});
+// SEARCH paper by Title
+$app->get('/search/paper/title/{title}',function(Request $request, Response $response){
+
+});
+// SEARCH paper by Authors
+$app->get('/search/paper/author/{author}',function(Request $request, Response $response){
+
+});
+//---------------------------------------------------------------------------------------
 // Run app
 $app->run();
